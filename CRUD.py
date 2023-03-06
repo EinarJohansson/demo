6194cab001b93b9594af2be6f7fc2b83c6dbaf00
Einar Johansson <ejrobb09@gmail.com>
2023-03-06 10:36:08 +0100

    Interaktiv

--- CRUD.py
+++ CRUD.py
@@ -43,21 +43,37 @@ class RedisDatabase:
         self.redis.delete(f'supplier:{supplier_id}')
 
     # CRUD operations for ProductSupplier table
-    def add_product_supplier(self, product_id, supplier_id):
+    def create_product_supplier(self, product_id, supplier_id):
         self.redis.sadd(f'product:{product_id}:suppliers', supplier_id)
-        self.redis.sadd(f'supplier:{supplier_id}:products', product_id)
+        #self.redis.sadd(f'supplier:{supplier_id}:products', product_id)
 
-    def remove_product_supplier(self, product_id, supplier_id):
+    def read_product_suppliers(self, supplier_id=None):
+        return self.redis.smembers(f'supplier:{supplier_id}:products')
+
+    def update_product_suppliers(self, product_id, supplier_id):
+        pass
+
+    def delete_product_supplier(self, product_id, supplier_id):
         self.redis.srem(f'product:{product_id}:suppliers', supplier_id)
-        self.redis.srem(f'supplier:{supplier_id}:products', product_id)
-
-    def get_product_suppliers(self, product_id):
-        return self.redis.smembers(f'product:{product_id}:suppliers')
+        #self.redis.delete(f'supplier:{supplier_id}:products')
 
     # CRUD operations for Orders table
     def create_order(self, product_id, quantity):
+        # Kolla om det går att beställa 
+        #key = f'product:{product_id}:quantity'
+        #og_quantity = int(self.redis.get(key))
+
+        og_quantity = int(self.read_product(product_id)[b'quantity'].decode('utf-8'))
+
+        if (int(quantity) > og_quantity):
+            raise Exception("Du har beställt för mycket!")
+
         order_id = self.redis.incr('order_id')
         self.redis.hmset(f'order:{order_id}', {'product_id': product_id, 'quantity': quantity})
+        
+        # Uppdaterar quantity
+        self.update_product(product_id, quantity=og_quantity-int(quantity))
+
         return order_id
 
     def read_order(self, order_id):
@@ -71,3 +87,32 @@ class RedisDatabase:
 
     def delete_order(self, order_id):
         self.redis.delete(f'order:{order_id}')
+
+    def show_all_products(self):
+        keys = self.redis.keys('product:*')
+        products = []
+        for key in keys:
+            if b':suppliers' not in key:
+                product = self.redis.hgetall(key)
+                key_id = key.decode('utf-8').split(':')[1]
+                products.append((product, key_id))
+        return products
+
+    def show_all_suppliers(self):
+        keys = self.redis.keys('supplier:*')
+        products = []
+        for key in keys:
+            if b':products' not in key:
+                product = self.redis.hgetall(key)
+                key_id = key.decode('utf-8').split(':')[1]
+                products.append((product, key_id))
+        return products
+
+    def show_all_product_suppliers(self, product_id):
+        keys = self.redis.smembers(f'product:{product_id}:suppliers')
+        products = []
+        for key in keys:
+            key = key.decode('utf-8')
+            sup = self.read_supplier(key)
+            products.append((sup, key))
+        return products
--- main.py
+++ main.py
@@ -1,51 +1,97 @@
 from CRUD import *
-
+import sys
 if __name__ == '__main__':
   # Create a RedisDatabase instance
   db = RedisDatabase()
 
+  print("\nCreate a product!")
+
+  product_name = input("Enter the productname: ")
+  product_quantity = input("Enter the quantity of the product: ")
+  product_price = input("Enter the price of the product (SEK): ")
+
   # Create a product
-  product_id = db.create_product('P001', 10, 100)
+  db.create_product(product_name, product_quantity, product_price)
+
+  print("\nCreate a supplier!")
+  supplier_name = input("Enter the suppliername: ")
+  supplier_phone = input("Enter the phonenumber for the supplier: ")
 
   # Create a supplier
-  supplier_id = db.create_supplier('ABC Supplies', '123-456-7890')
+  db.create_supplier(supplier_name, supplier_phone)
+
+  print("\nCreate a product supplier!")
+  
+  print("Here is all the products!")
+  for (product, key) in db.show_all_products():
+    print('Product ID:')
+    print(key)
+    print('Value:')
+    print(product)
+    print()
+  
+  chosen_product_id = input("Choose a product_id that the supplier should stock: ")
+
+  print("\nHere are all the suppliers!")
+  for (supplier, key) in db.show_all_suppliers():
+    print('Supplier ID:')
+    print(key)
+    print('Value:')
+    print(supplier)
+    print()
+
+  chosen_supplier_id = input("Choose what supplier that should stock the product: ")
 
   # Associate the product with the supplier
-  db.add_product_supplier(product_id, supplier_id)
+  db.create_product_supplier(chosen_product_id, chosen_supplier_id)
+
+  print('\nCreate an order!')
+  for (product, key) in db.show_all_products():
+    print('Product ID:')
+    print(key)
+    print('Value:')
+    print(product)
+    print()
+
+  order_product_id = input("Which product do you want to order?: ")
+
+  for (supplier, key) in db.show_all_product_suppliers(order_product_id):
+    print('Supplier ID:')
+    print(key)
+    print('Value:')
+    print(supplier)
+    print()
+
+  order_supplier_id = input("Which supplier do you want to order from?: ")
 
   # Create an order for the product
-  order_id = db.create_order(product_id, 5)
+  quantity = input("How much of the product do you want to order?: ")
+  order_id = db.create_order(order_product_id, quantity)
 
-  # Read the product, supplier, and order
-  product = db.read_product(product_id)
-  supplier = db.read_supplier(supplier_id)
-  order = db.read_order(order_id)
-
-  print('Product:', product)
-  print('Supplier:', supplier)
-  print('Order:', order)
-
-  # Update the product and order
-  db.update_product(product_id, quantity=20)
-  db.update_order(order_id, quantity=10)
+  print('\nDin beställning är klar!')
 
   # Read the updated product and order
-  product = db.read_product(product_id)
+  product = db.read_product(order_product_id)
   order = db.read_order(order_id)
 
-  print('Updated Product:', product)
-  print('Updated Order:', order)
+  print('Uppdaterade produkten:', product)
+  print('Din order:', order)
 
   # Delete the product and order
-  db.delete_product(product_id)
+  db.delete_product(chosen_product_id)
   db.delete_order(order_id)
+  db.delete_product_supplier(chosen_product_id, chosen_supplier_id)
+  db.delete_supplier(chosen_supplier_id)
 
   # Check if the product and order still exist
-  product = db.read_product(product_id)
+  product = db.read_product(chosen_product_id)
   order = db.read_order(order_id)
+  supplier = db.read_supplier(chosen_supplier_id)
+  product_supplier = db.read_product_suppliers(chosen_supplier_id)
 
   print('Product after deletion:', product)
   print('Order after deletion:', order)
+  print('Supplier after deletion:', supplier)
+  print('Product supplier after deletion:', product_supplier)
 
 
-  
